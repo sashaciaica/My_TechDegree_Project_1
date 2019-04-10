52edc5ceff128f27faad8ef899fb2ceb345ad6cf
sashaciaica <48767248+sashaciaica@users.noreply.github.com>
2019-04-10 14:56:03 +0100

    Update Project_1_Number_Guessing_Name.py

--- Project_1_Number_Guessing_Name.py
+++ Project_1_Number_Guessing_Name.py
@@ -1,17 +1,23 @@
 from random import randint
-guess = int(input("Welcome: Please type a random number 1-10: "))
 ran_num = randint(1, 10)  # Pick a random number between 1 and 10.
-guesses = 1
 
-while guess != ran_num:
-    if guess > ran_num:
-        print("Your guess is too high")
-        guess = int(input("Welcome: Please type a random number 1-10: "))
-        guesses = guesses + 1
-    elif guess < ran_num:
-        print("Your guess is too low")
-        guess = int(input("Welcome: Please type a random number 1-10: "))
-        guesses = guesses + 1
+def start_game():
+    guess = int(input("Welcome: Please type a random number 1-10: "))
+    guesses = 1
+    while guess != ran_num:
+        if guess > ran_num:
+            print("Your guess is too high")
+            guess = int(input("Welcome: Please type a random number 1-10: "))
+            guesses = guesses + 1
+        elif guess < ran_num:
+            print("Your guess is too low")
+            guess = int(input("Welcome: Please type a random number 1-10: "))
+            guesses = guesses + 1
+    
+    print("Success")
+    print("It took you ", guesses, "of guesses")
 
-print("Success")
-print("It took you ", guesses, "of guesses")
+
+if __name__ == '__main__':
+    # Kick off the program by calling the start_game function.
+    start_game()
