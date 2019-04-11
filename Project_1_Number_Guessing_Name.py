79c820834925880776a0fcf382984c55e297ea23
sashaciaica <48767248+sashaciaica@users.noreply.github.com>
2019-04-11 23:02:23 +0100

    Update Project_1_Number_Guessing_Name.py

--- Project_1_Number_Guessing_Name.py
+++ Project_1_Number_Guessing_Name.py
@@ -1,21 +1,29 @@
 from random import randint
+# starting_range = int(input("Please enter STARTING range: "))
+# ending_range = int(input("Please enter ENDING range: "))
 ran_num = randint(1, 10)  # Pick a random number between 1 and 10.
+# print("Your range is ", starting_range , "and", ending_range)
 
 def start_game():
-    guess = int(input("Welcome: Please type a random number 1-10: "))
-    guesses = 1
-    while guess != ran_num:
-        if guess > ran_num:
-            print("Your guess is too high")
-            guess = int(input("Welcome: Please type a random number 1-10: "))
-            guesses = guesses + 1
-        elif guess < ran_num:
-            print("Your guess is too low")
-            guess = int(input("Welcome: Please type a random number 1-10: "))
-            guesses = guesses + 1
-    
-    print("Success")
-    print("It took you ", guesses, "of guesses")
+    try:
+        guess = int(input("Welcome: Please type a random number between 1 and 10 "))
+        guesses = 1
+
+        while guess != ran_num:
+            if guess > ran_num:
+                print("Your guess is too high")
+                guess = int(input("Welcome: Please type a random number between 1 and 10 "))
+                guesses = guesses + 1
+            elif guess < ran_num:
+                print("Your guess is too low")
+                guess = int(input("Welcome: Please type a random number between 1 and 10 "))
+                guesses = guesses + 1
+
+    except:
+        print("Ooops. I said a number")
+    else:
+        print("Success")
+        print("It took you ", guesses, "of guesses")
 
 
 if __name__ == '__main__':
